bb0227769c2d98b996066d8a689d7b87be8dd0b3
kshitij-404 <kshitij4ka@gmail.com>
2024-03-17 14:35:17 +0530

    feature/cron

--- src/utils/cancelTxnCron.ts
+++ src/utils/cancelTxnCron.ts
@@ -12,14 +12,11 @@ const cancelTxnCron = cron({
                     paymentStatus: 'pending',
                     placedAt: { $lt: new Date(new Date().getTime() - 5 * 60 * 1000) }
                 }).exec();
-                console.log("pendingPaymentOrders:", pendingPaymentOrders);
-                console.log(new Date("2024-03-15T05:53:47.492+00:00").getTime())
                 await Promise.all(pendingPaymentOrders.map(async (o) => {
                     try {
                         const urlParts = o.paymentMetadata.data.payment_url.split("/")
                         const paymentId = urlParts[urlParts.length - 1]
                         await ky.get(`https://qrstuff.me/gateway/cancel_txn/${paymentId}`)
-                        o.paymentStatus = 'failed';
                         await o.save();
                     }
                     catch (error) {
