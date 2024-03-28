f96079ea75caa7d05db8a6e9af6c56f154f59c91
Raj Varsani <varasaniraj5@gmail.com>
2024-03-28 11:06:52 +0530

    Refactor user model schema

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,27 +1,26 @@
-import mongoose, { Schema, Document } from 'mongoose';
+import mongoose, { Schema, Document } from "mongoose";
 
 // Define the user schema
 interface IUser extends Document {
-    name: string;
-    email: string;
-    avatar: string;
+  name: string;
+  email: string;
+  avatar: string;
 }
 
 const UserSchema: Schema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
 });
 
 // Create and export the user model
-const UserModel = mongoose.model<IUser>('User', UserSchema);
+const UserModel = mongoose.model<IUser>("User", UserSchema);
 export default UserModel;
